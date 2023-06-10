a4c1fcecbdb280b1d62e7e02272c6c68727668e5
LimTaeHun <lsw463@naver.com>
2023-06-10 18:13:04 +0900

    Feat: TopDownCamera 필드 변수와 DrawLine 구현

--- Assets/01. Scripts/Camera/TopDownCamera.cs
+++ Assets/01. Scripts/Camera/TopDownCamera.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class TopDownCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float height = 5f;
+    public float distance = 10f;
+    public float angle = 45f;
+    public float lookAtHeight = 2f;
+    public float smoothSpeed = 0.5f;
+
+    public Transform target;
+    
+    private Vector3 refVelocity;
+
+    private void LateUpdate()
     {
-        
+        HandleCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleCamera()
     {
-        
+        if (!target)
+        {
+            return;
+        }
+
+        Vector3 worldPositions = (Vector3.forward * -distance) + (Vector3.up * height);
+        Debug.DrawLine(target.position, worldPositions, Color.red);
     }
 }
