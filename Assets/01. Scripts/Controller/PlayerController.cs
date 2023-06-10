8c541a5a30dafb1daeee03f603761294935098d8
LimTaeHun <lsw463@naver.com>
2023-06-10 12:39:41 +0900

    Feat: 메인 카메라 피킹을 이용한 클릭시 오브젝트 정보 출력 기능 구현

--- Assets/01. Scripts/Controller/PlayerController.cs
+++ Assets/01. Scripts/Controller/PlayerController.cs
@@ -1,66 +1,48 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
     // 컨트롤러 속성 값
-    public float speed = 5f;
-    public float jumpHeight = 2f;
-    public float dashDistance = 5f;
-    public float gravity = -9.81f;
-    public Vector3 drags;
-    
     private Vector3 _calcVelocity;
     
     // Ground 체크
-    public bool _isGround;
+    public LayerMask groundLayerMast;
+    public float groundCheckDistance = 0.3f;
+    private bool _isGround;
     
     // 컴포넌트
     private CharacterController _controller;
+    private NavMeshAgent _agent;
+    private Camera _camera;
 
     private void Start()
     {
+        // CharacterController 컴포넌트를 얻음
         _controller = GetComponent<CharacterController>();
+        
+        // NavMeshAgent 컴포넌트를 얻음
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updatePosition = false;
+        _agent.updateRotation = true;
+        
+        // 메인 카메라를 얻음
+        _camera = Camera.main;
     }
 
     private void Update()
     {
-        _isGround = _controller.isGrounded;
-
-        if (_isGround && _calcVelocity.y < 0)
+        // 사용자가 마우스 왼쪽 클릭 했을 때
+        if (Input.GetMouseButtonDown(0))
         {
-            _calcVelocity.y = 0;
+            // 카메라 피킹 사용 
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, groundLayerMast))
+            {
+            }
         }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _controller.Move(move * (Time.deltaTime * speed));
-
-        if (move != Vector3.zero)
-        {
-            transform.forward = move;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && !_isGround)
-        {
-            _calcVelocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            float value = (Mathf.Log(1f / (Time.deltaTime * drags.x + 1)) / -Time.deltaTime);
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3(value,0f,value));
-            _calcVelocity += dashVelocity;
-        }
-
-        // 중력 계산
-        _calcVelocity.y += gravity * Time.deltaTime;
-        
-        // 대쉬의 drag 계산
-        _calcVelocity.x /= 1 + drags.x * Time.deltaTime;
-        _calcVelocity.y /= 1 + drags.y * Time.deltaTime;
-        _calcVelocity.z /= 1 + drags.z * Time.deltaTime;
-
-        _controller.Move(_calcVelocity * Time.deltaTime);
     }
 }
