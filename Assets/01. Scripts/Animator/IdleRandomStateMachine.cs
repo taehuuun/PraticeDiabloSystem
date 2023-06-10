c680bb8bcdef833a48408e8991d18db6d8da9645
LimTaeHun <lsw463@naver.com>
2023-06-10 11:14:16 +0900

    Feat: YBot, Idle 애니메이션 랜덤 전환 기능 구현

--- Assets/01. Scripts/Animator/IdleRandomStateMachine.cs
+++ Assets/01. Scripts/Animator/IdleRandomStateMachine.cs
@@ -1,20 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IdleRandomStateMachine : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public float minNormalizeTime = 2f;
+    public float maxNormalizeTime = 5f;
+    public float randomNormalizeTime;
+    
+    public int idleStates = 3;
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    private readonly int _hashRandomIdle = Animator.StringToHash("RandomIdle");
+
+     // OnStateEnter는 전환이 시작되고 상태 시스템이 이 상태를 평가하기 시작하면 호출됩니다
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // 트랜지션의 필요한 시간을 랜덤하게 결정
+        randomNormalizeTime = Random.Range(minNormalizeTime, maxNormalizeTime);
+    }
+
+    // OnStateEnter 콜백과 OnStateExit 콜백 사이의 각 업데이트 프레임에서 OnStateUpdate가 호출됩니다
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // 이 State에서 벗어나 전환되는 경우 Idle 랜덤 매개 변수를 -1로 재설정합니다.
+        if (animator.IsInTransition(0) &&
+            animator.GetCurrentAnimatorStateInfo(0).fullPathHash == stateInfo.fullPathHash)
+        {
+            animator.SetInteger(_hashRandomIdle, -1);
+        }
+        
+        // State가 Normalize 시간을 랜덤하게 결정 후 아직 전환 되지 않는 경우
+        if (stateInfo.normalizedTime > randomNormalizeTime && !animator.IsInTransition(0))
+        {
+            animator.SetInteger(_hashRandomIdle, Random.Range(0,idleStates));
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
