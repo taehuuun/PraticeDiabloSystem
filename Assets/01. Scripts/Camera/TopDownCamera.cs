6108ee471dd1989491b7441fdfc0c180cd8dafb6
LimTaeHun <lsw463@naver.com>
2023-06-11 13:55:05 +0900

    Feat: 커스텀 에디터 확장 구현

--- Assets/01. Scripts/Camera/TopDownCamera.cs
+++ Assets/01. Scripts/Camera/TopDownCamera.cs
@@ -29,18 +29,15 @@ public class TopDownCamera : MonoBehaviour
         
         // 월드 포지션 벡터 생성
         Vector3 worldPositions = (Vector3.forward * -distance) + (Vector3.up * height);
-        Debug.DrawLine(target.position, worldPositions, Color.red);
         
         // 회전 벡터
         Vector3 rotateVector = Quaternion.AngleAxis(angle, Vector3.up) * worldPositions;
-        Debug.DrawLine(target.position, rotateVector, Color.green);
         
         // 이동 벡터
         Vector3 finalTargetPosition = target.position;
         finalTargetPosition.y += lookAtHeight;
 
         Vector3 finalPosition = finalTargetPosition + rotateVector;
-        Debug.DrawLine(target.position, finalPosition, Color.magenta);
 
         transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothSpeed);
         transform.LookAt(target.position);
--- Assets/01. Scripts/Editor/TopDownCameraEditor.cs
+++ Assets/01. Scripts/Editor/TopDownCameraEditor.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class TopDownCameraEditor : MonoBehaviour
+[CustomEditor(typeof(TopDownCamera))]
+public class TopDownCameraEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    private TopDownCamera _topDownCamera;
+
+    public override void OnInspectorGUI()
     {
-        
+        _topDownCamera = (TopDownCamera)target;
+        base.OnInspectorGUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneGUI()
     {
+        if (!_topDownCamera || !_topDownCamera.target)
+        {
+            return;
+        }
+
+        Transform cameraTarget = _topDownCamera.target;
+        Vector3 targetPosition = cameraTarget.position;
+        targetPosition.y += _topDownCamera.lookAtHeight;
         
+        // Distance를 나타내는 구를 그림
+        Handles.color = new Color(1f, 0f, 0f, 0.15f);
+        Handles.DrawSolidDisc(targetPosition, Vector3.up, _topDownCamera.distance);
+        
+        Handles.color = new Color(0f, 1f, 0f, 0.75f);
+        Handles.DrawWireDisc(targetPosition,Vector3.up,_topDownCamera.distance);
+        
+        // Handle의 Distance 슬라이더 추가
+        Handles.color = new Color(1f, 0f, 0f, 0.5f);
+        _topDownCamera.distance = Handles.ScaleSlider(_topDownCamera.distance, targetPosition, -cameraTarget.forward,
+            Quaternion.identity, _topDownCamera.distance, 0.1f);
+        _topDownCamera.distance = Mathf.Clamp(_topDownCamera.distance, 2f, float.MaxValue);
+        
+        // Handle의 Height 슬라이더 추가
+        Handles.color = new Color(0f, 0f, 1f);
+        _topDownCamera.height = Handles.ScaleSlider(_topDownCamera.height, targetPosition, Vector3.up,
+            Quaternion.identity, _topDownCamera.height, 0.1f);
+        _topDownCamera.height = Mathf.Clamp(_topDownCamera.height, 2f, float.MaxValue);
+
+        // 라벨을 생성
+        GUIStyle labelStyle = new GUIStyle();
+        labelStyle.fontSize = 15;
+        labelStyle.normal.textColor = Color.white;
+        labelStyle.alignment = TextAnchor.UpperCenter;
+        
+        // Distance 라벨
+        Handles.Label(targetPosition + (-cameraTarget.forward * _topDownCamera.distance), "Distance", labelStyle);
+        
+        // Height 레벨
+        labelStyle.alignment = TextAnchor.MiddleRight;
+        Handles.Label(targetPosition + (Vector3.up * _topDownCamera.height),"Height",labelStyle);
+        
+        _topDownCamera.HandleCamera();
     }
 }
