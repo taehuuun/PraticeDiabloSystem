c16d57677559f77af7187e8a9d25063027dac917
LimTaeHun <lsw463@naver.com>
2023-06-11 19:10:44 +0900

    Feat: Set메서드와 가상, 추상 메서드 추가

--- Assets/01. Scripts/AI/State.cs
+++ Assets/01. Scripts/AI/State.cs
@@ -1,4 +1,21 @@
 public abstract class State<T>
 {
     protected StateMachine<T> stateMachine;
+    protected T context;
+
+    public State()
+    {
+        
+    }
+
+    internal void SetStateMachineAndContext(StateMachine<T> stateMachine, T context)
+    {
+        this.stateMachine = stateMachine;
+        this.context = context;
+        OnInitialized();
+    }
+    public virtual void OnInitialized() { }
+    public virtual void OnEnter() { }
+    public abstract void Update(float deltaTime);
+    public virtual void OnExit() { }
 }
