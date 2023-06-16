7c7487f3da8c72a6affaa14b00ce94440222e1ed
LimTaeHun <lsw463@naver.com>
2023-06-17 06:49:19 +0900

    Feat: StateMachine 기본 메서드 구현

--- Assets/01. Scripts/AI/StateMachine.cs
+++ Assets/01. Scripts/AI/StateMachine.cs
@@ -1,3 +1,54 @@
-public class StateMachine<T>
+using System;
+using System.Collections.Generic;
+
+public sealed class StateMachine<T>
 {
+    public State<T> CurrentState { get; private set; }
+    public State<T> PrevState { get; private set; }
+    public float ElapsedTimeInState { get; private set; }
+
+    private Dictionary<Type, State<T>> _states = new Dictionary<Type, State<T>>();
+    private T _context;
+
+    public StateMachine(T context, State<T> initializeState)
+    {
+        _context = context;
+        AddState(initializeState);
+
+        CurrentState = initializeState;
+        CurrentState.OnEnter();
+    }
+
+    public void AddState(State<T> state)
+    {
+        state.SetStateMachineAndContext(this, _context);
+        _states.Add(state.GetType(), state);
+    }
+    public void Update(float deltaTime)
+    {
+        ElapsedTimeInState += deltaTime;
+        CurrentState.Update(deltaTime);
+    }
+    public R ChangeState<R>() where R : State<T>
+    {
+        var newType = typeof(R);
+
+        if (CurrentState.GetType() == newType)
+        {
+            return CurrentState as R;
+        }
+
+        if (CurrentState != null)
+        {
+            CurrentState.OnExit();
+        }
+
+        PrevState = CurrentState;
+        CurrentState = _states[newType];
+        
+        CurrentState.OnEnter();
+        ElapsedTimeInState = 0f;
+
+        return CurrentState as R;
+    }
 }
