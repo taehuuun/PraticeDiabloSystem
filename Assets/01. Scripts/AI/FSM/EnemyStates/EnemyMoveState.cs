c2a94858acfca637083f80a1e591c31bb9cacf2a
LimTaeHun <lsw463@naver.com>
2023-06-23 17:33:19 +0900

    Feat: EnemyMoveState의 OnInitialized, OnEnter, Update, OnExit 메서드 구현

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyMoveState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyMoveState.cs
@@ -1,6 +1,54 @@
+using UnityEngine;
+using UnityEngine.AI;
+
 public class EnemyMoveState : State<EnemyController>
 {
+    private Animator _animator;
+    private CharacterController _controller;
+    private NavMeshAgent _agent;
+
+    private int _hashMove = Animator.StringToHash("Move");
+    private int _hashMoveSpeed = Animator.StringToHash("MoveSpeed");
+    
+    public override void OnInitialized()
+    {
+        _animator = context.GetComponent<Animator>();
+        _controller = context.GetComponent<CharacterController>();
+        _agent = context.GetComponent<NavMeshAgent>();
+    }
+
+    public override void OnEnter()
+    {
+        _agent.SetDestination(context.target.position);
+        _animator.SetBool(_hashMove, true);
+    }
+
     public override void Update(float deltaTime)
     {
+        Transform enemy = context.SearchEnemy();
+
+        if (enemy)
+        {
+            _agent.SetDestination(context.target.position);
+
+            if (_agent.remainingDistance > _agent.stoppingDistance)
+            {
+                _controller.Move(_agent.velocity * Time.deltaTime);
+                _animator.SetFloat(_hashMoveSpeed, _agent.velocity.magnitude / _agent.speed,1f, deltaTime);
+                return;
+            }
+        }
+
+        if (!enemy || _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            stateMachine.ChangeState<EnemyIdleState>();
+        }
+    }
+
+    public override void OnExit()
+    {
+        _animator.SetBool(_hashMove, false);
+        _animator.SetFloat(_hashMoveSpeed, 0f);
+        _agent.ResetPath();
     }
 }
