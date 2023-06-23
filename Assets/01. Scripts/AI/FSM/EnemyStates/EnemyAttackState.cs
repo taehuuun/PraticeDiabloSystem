b6599c88b89cc3eac245fc215794e4aae40f0f00
LimTaeHun <lsw463@naver.com>
2023-06-23 17:57:13 +0900

    Feat: OnInitialized, OnEnter 메서드 오버라이드 구현

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
@@ -1,5 +1,28 @@
+using UnityEngine;
+
 public class EnemyAttackState : State<EnemyController>
 {
+    private Animator _animator;
+
+    private int _hashAttack = Animator.StringToHash("Attack");
+
+    public override void OnInitialized()
+    {
+        _animator = context.GetComponent<Animator>();
+    }
+
+    public override void OnEnter()
+    {
+        if (context.isAbleAttack)
+        {
+            _animator.SetTrigger(_hashAttack);
+        }
+        else
+        {
+            stateMachine.ChangeState<EnemyIdleState>();
+        }
+    }
+
     public override void Update(float deltaTime)
     {
     }
