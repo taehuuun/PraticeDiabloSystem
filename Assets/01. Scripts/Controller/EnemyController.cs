56a2510512012e3fb611e7ad5397661c9467acba
LimTaeHun <lsw463@naver.com>
2023-06-23 11:08:20 +0900

    Feat: SearchEnemy 메서드 구현

--- Assets/01. Scripts/Controller/EnemyController.cs
+++ Assets/01. Scripts/Controller/EnemyController.cs
@@ -3,4 +3,50 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     protected StateMachine<EnemyController> stateMachine;
+
+    public Transform target;
+    public LayerMask targetLayerMask;
+    public float viewRadius;
+    public float attackRange;
+
+    public bool isAbleAttack
+    {
+        get
+        {
+            if (!target)
+            {
+                return false;
+            }
+
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            return distance <= attackRange;
+        }
+    }
+    
+    private void Start()
+    {
+        stateMachine = new StateMachine<EnemyController>(this,new EnemyIdleState());
+        stateMachine.AddState(new EnemyMoveState());
+        stateMachine.AddState(new EnemyAttackState());
+    }
+
+    private void Update()
+    {
+        stateMachine.Update(Time.deltaTime);
+    }
+
+    public Transform SearchEnemy()
+    {
+        target = null;
+        Collider[] targetViews = null;
+        Physics.OverlapSphereNonAlloc(transform.position, viewRadius, targetViews, targetLayerMask);
+
+        if (targetViews.Length > 0)
+        {
+            target = targetViews[0].transform;
+        }
+
+        return target;
+    }
 }
