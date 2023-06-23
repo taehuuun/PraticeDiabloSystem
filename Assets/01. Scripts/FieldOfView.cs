0ee2e951fff26b17a2e18f5bdcaa15eb63fc49df
LimTaeHun <lsw463@naver.com>
2023-06-23 23:43:12 +0900

    Feat: 가시선 구현 및 가시선 내 타겟 찾는 기능 구현

--- Assets/01. Scripts/FieldOfView.cs
+++ Assets/01. Scripts/FieldOfView.cs
@@ -1,18 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float viewRadius = 5f;
 
-    // Update is called once per frame
-    void Update()
+    [Range(0, 360)] public float viewAngle = 90f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    private List<Transform> _detectTargets = new List<Transform>();
+    private Transform _nearestTarget;
+    
+    private float _distanceTarget;
+
+    private void Update()
     {
-        
+        FindDetectTargets();
+    }
+    
+    private void FindDetectTargets()
+    {
+        _distanceTarget = 0f;
+        _nearestTarget = null;
+        _detectTargets.Clear();
+
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distance = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distance, obstacleMask))
+                {
+                    _detectTargets.Add(target);
+
+                    if (_nearestTarget == null || (_distanceTarget > distance))
+                    {
+                        _nearestTarget = target;
+                        _distanceTarget = distance;
+                    }
+                }
+            }
+        }
     }
 }
