d5429bc6799fa9b8ec85eb70edf0bb27b1beaa2f
LimTaeHun <lsw463@naver.com>
2023-06-23 11:07:54 +0900

    Feat: 각 클래스 State 상속 및 필수 구현 메서드 추가

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyAttackState : MonoBehaviour
+public class EnemyAttackState : State<EnemyController>
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Update(float deltaTime)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyMoveState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyMoveState.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyMoveState : MonoBehaviour
+public class EnemyMoveState : State<EnemyController>
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Update(float deltaTime)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
