07e1156372c836fefb924c93b72b55e1745784af
LimTaeHun <lsw463@naver.com>
2023-06-24 10:19:41 +0900

    Feat: OnStateEnter, OnStateExit 구현

--- Assets/01. Scripts/AI/AnimatorStateMachine/AttackStateMachineBehaviour.cs
+++ Assets/01. Scripts/AI/AnimatorStateMachine/AttackStateMachineBehaviour.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class AttackStateMachineBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<AttackStateController>()?.OnStateOfAttackState();
+    }
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<AttackStateController>()?.OnEndOfAttackState();
+    }
 
     // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -16,11 +21,6 @@ public class AttackStateMachineBehaviour : StateMachineBehaviour
     //    
     //}
 
-    // OnStateExit is called before OnStateExit is called on any state inside this state machine
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
     // OnStateMove is called before OnStateMove is called on any state inside this state machine
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
