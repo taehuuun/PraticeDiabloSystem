b961b09269e7f13b2248a362ade2bdb437a7dbb0
LimTaeHun <lsw463@naver.com>
2023-06-24 17:26:41 +0900

    Feat: EnemyDeadState 구현

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyDeadState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyDeadState.cs
@@ -1,3 +1,26 @@
-public class EnemyDeadState : EnemyController
+using UnityEngine;
+
+public class EnemyDeadState : State<EnemyController>
 {
+    private Animator _animator;
+
+    protected int hashIsAlive = Animator.StringToHash("isAlive");
+    
+    public override void OnInitialized()
+    {
+        _animator = context.GetComponent<Animator>();
+    }
+
+    public override void OnEnter()
+    {
+        _animator.SetBool(hashIsAlive, false);
+    }
+
+    public override void Update(float deltaTime)
+    {
+        if (stateMachine.ElapsedTimeInState > 3.0f)
+        {
+            GameObject.Destroy(context.gameObject);
+        }
+    }
 }
