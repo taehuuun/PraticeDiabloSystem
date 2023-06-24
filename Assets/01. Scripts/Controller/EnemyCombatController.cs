5b102eba3a31f332b125c94ad88c5271fd27c438
LimTaeHun <lsw463@naver.com>
2023-06-24 17:20:27 +0900

    Feat: EnemyController 상속 후 Start 가상 메서드 오버라이드 구현

--- Assets/01. Scripts/Controller/EnemyCombatController.cs
+++ Assets/01. Scripts/Controller/EnemyCombatController.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyCombatController : MonoBehaviour
+public class EnemyCombatController : EnemyController
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Start();
+        StateMachine.AddState(new EnemyMoveState());
+        StateMachine.AddState(new EnemyAttackState());
     }
 }
