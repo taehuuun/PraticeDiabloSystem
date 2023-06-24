5bc42cb1bac119efbc093ee1e742dc21fc6c556b
LimTaeHun <lsw463@naver.com>
2023-06-24 17:05:00 +0900

    Feat: AttackBehaviour 구현

--- Assets/01. Scripts/AI/AttackBehaviour.cs
+++ Assets/01. Scripts/AI/AttackBehaviour.cs
@@ -2,4 +2,33 @@ using UnityEngine;
 
 public abstract class AttackBehaviour : MonoBehaviour
 {
+    #if UNITY_EDITOR
+    [Multiline] public string description;
+    #endif
+
+    public GameObject effect;
+    public LayerMask targetMask;
+    
+    public int animationIdx;
+    public int priority;
+    public int damage = 10;
+    public float range = 3f;
+
+    [SerializeField] protected float coolTime;
+    protected float calCoolTime = 0f;
+    
+    private void Start()
+    {
+        calCoolTime = coolTime;
+    }
+
+    private void Update()
+    {
+        if (calCoolTime < coolTime)
+        {
+            calCoolTime += Time.deltaTime;
+        }
+    }
+
+    public abstract void ExecuteAttack(GameObject target = null, Transform startPoint = null);
 }
