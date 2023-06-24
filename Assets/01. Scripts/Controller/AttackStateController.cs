3f59359e1660256053606ab7aacbafa9d91a11c2
LimTaeHun <lsw463@naver.com>
2023-06-24 10:08:19 +0900

    Feat: delegate 추가 및 관련 메서드, 프로퍼티 추가 구현

--- Assets/01. Scripts/Controller/AttackStateController.cs
+++ Assets/01. Scripts/Controller/AttackStateController.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class AttackStateController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public delegate void OnEnterAttackState();
+    public delegate void OnExitAttackState();
+
+    public OnEnterAttackState enterAttackStateController;
+    public OnExitAttackState exitAttackStateController;
+    
+    public bool IsInAttackState { get; private set; }
+
+    private void Start()
+    {
+        enterAttackStateController = EnterAttackState;
+        exitAttackStateController = ExitAttackState;
+    }
+
+    public void OnStateOfAttackState()
+    {
+        IsInAttackState = true;
+        enterAttackStateController?.Invoke();
+    }
+
+    public void OnEndOfAttackState()
+    {
+        IsInAttackState = false;
+        exitAttackStateController?.Invoke();
+    }
+    
+    private void EnterAttackState()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ExitAttackState()
     {
         
     }
