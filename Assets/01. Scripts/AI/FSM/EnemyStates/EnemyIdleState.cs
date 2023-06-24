5a952beead2a357137f09dd50ccf333f37b2d0e2
LimTaeHun <lsw463@naver.com>
2023-06-24 09:39:57 +0900

    Modify: Patrol 상태에 따른 Update 분기 수정

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyIdleState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyIdleState.cs
@@ -8,6 +8,11 @@ public class EnemyIdleState : State<EnemyController>
     protected readonly int isMove = Animator.StringToHash("Move");
     protected readonly int moveSpeed = Animator.StringToHash("MoveSpeed");
     
+    private bool _isPatrol;
+    private float _minIdleTime = 0f;
+    private float _maxIdleTime = 3.0f;
+    private float _idleTime = 0f;
+    
     public override void OnInitialized()
     {
         _animator = context.GetComponent<Animator>();
@@ -19,6 +24,11 @@ public class EnemyIdleState : State<EnemyController>
         _animator?.SetBool(isMove, false);
         _animator?.SetFloat(moveSpeed, 0f);
         _controller?.Move(Vector3.zero);
+
+        if (_isPatrol)
+        {
+            _idleTime = Random.Range(_minIdleTime, _maxIdleTime);
+        }
     }
 
     public override void Update(float deltaTime)
@@ -35,6 +45,10 @@ public class EnemyIdleState : State<EnemyController>
             {
                 stateMachine.ChangeState<EnemyMoveState>();
             }
+        }
+        else if(_isPatrol && stateMachine.ElapsedTimeInState > _idleTime)
+        {
+            stateMachine.ChangeState<EnemyMoveToWayPoint>();
         }
     }
 }
