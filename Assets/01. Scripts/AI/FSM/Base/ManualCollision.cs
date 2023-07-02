df8ea577d25d651b01ba1c81f86b68c8db22a6f2
LimTaeHun <lsw463@naver.com>
2023-07-02 10:00:32 +0900

    Feat: CheckOverlapBox 메서드, OnDrawGizmos 메서드 구현

--- Assets/01. Scripts/AI/FSM/Base/ManualCollision.cs
+++ Assets/01. Scripts/AI/FSM/Base/ManualCollision.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public class ManualCollision : MonoBehaviour
 {
+    public Vector3 boxSize = new(3, 2, 2);
+
+    public Collider[] CheckOverlapBox(LayerMask layerMask)
+    {
+        return Physics.OverlapBox(transform.position, boxSize * 0.5f,transform.rotation, layerMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
+    }
 }
