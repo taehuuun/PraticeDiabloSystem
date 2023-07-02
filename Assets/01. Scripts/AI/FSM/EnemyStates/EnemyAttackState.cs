1cdca7807c068e133ede0dd1ee99e6f516a13ede
LimTaeHun <lsw463@naver.com>
2023-07-02 09:37:18 +0900

    Feat: EnemyAttackState와 EnemyCombatController 연동 작업

--- Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
+++ Assets/01. Scripts/AI/FSM/EnemyStates/EnemyAttackState.cs
@@ -3,27 +3,45 @@ using UnityEngine;
 public class EnemyAttackState : State<EnemyController>
 {
     private Animator _animator;
+    private AttackStateController _attackStateController;
+    private IAttackable _attackable;
 
     private int _hashAttack = Animator.StringToHash("Attack");
+    private int _hashAttackIdx = Animator.StringToHash("AttackIdx");
 
     public override void OnInitialized()
     {
         _animator = context.GetComponent<Animator>();
+        _attackStateController = context.GetComponent<AttackStateController>();
+        _attackable = context.GetComponent<IAttackable>();
     }
 
     public override void OnEnter()
     {
-        if (context.IsAbleAttack)
-        {
-            _animator.SetTrigger(_hashAttack);
-        }
-        else
+        if (_attackable ==null || Equals(_attackable.CurAttackBehaviour, null))
         {
             stateMachine.ChangeState<EnemyIdleState>();
+            return;
         }
-    }
 
+        _attackStateController.enterAttackStateController += OnEnterAttackState;
+        _attackStateController.exitAttackStateController += OnExitAttackState;
+        
+        _animator?.SetInteger(_hashAttackIdx, _attackable.CurAttackBehaviour.animationIdx);
+        _animator.SetTrigger(_hashAttack);
+    }
+    
     public override void Update(float deltaTime)
     {
+    }
+
+    public void OnEnterAttackState()
+    {
+        
+    }
+
+    public void OnExitAttackState()
+    {
+        stateMachine.ChangeState<EnemyIdleState>();
     }
 }
