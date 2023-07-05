b0a014bd89b96cc1d5bfedf08d1362989747e6a6
LimTaeHun <lsw463@naver.com>
2023-07-05 17:54:03 +0900

    Feat: ProjectileAttackBehaviour 구현

--- Assets/01. Scripts/AI/FSM/Behaviour/ProjectileAttackBehaviour.cs
+++ Assets/01. Scripts/AI/FSM/Behaviour/ProjectileAttackBehaviour.cs
@@ -1,5 +1,29 @@
 using UnityEngine;
 
-public class ProjectileAttackBehaviour : MonoBehaviour
+public class ProjectileAttackBehaviour : AttackBehaviour
 {
+    public override void ExecuteAttack(GameObject target = null, Transform startPoint = null)
+    {
+        if (!target)
+        {
+            return;
+        }
+
+        Vector3 projectilePosition = startPoint?.position ?? transform.position;
+
+        if (effect)
+        {
+            Projectile projectile = Instantiate(effect, projectilePosition, Quaternion.identity).GetComponent<Projectile>();
+            projectile.transform.forward = transform.forward;
+
+            if (projectile)
+            {
+                projectile.owner = this.gameObject;
+                projectile.target = target;
+                projectile.attackBehaviour = this;
+            }
+        }
+
+        calCoolTime = 0.0f;
+    }
 }
