c94557699e1688c8a2f0f3d1c38547232bdcd645
LimTaeHun <lsw463@naver.com>
2023-07-05 15:11:45 +0900

    Feat: DestoryParticle 코루틴 메서드 구현

--- Assets/01. Scripts/AI/FSM/Base/Projectile.cs
+++ Assets/01. Scripts/AI/FSM/Base/Projectile.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -113,5 +115,39 @@ public class Projectile : MonoBehaviour
                 }
             }
         }
+
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+
+        if (damageable!=null)
+        {
+            damageable.TakeDamage(attackBehaviour?.damage ?? 0, null);
+        }
+    }
+
+    private IEnumerator DestoryParticle(float waitTime)
+    {
+        if (transform.childCount > 0 && waitTime != 0)
+        {
+            List<Transform> childs = new List<Transform>();
+
+            foreach (Transform child in transform.GetChild(0).transform)
+            {
+                childs.Add(child);
+            }
+
+            while (transform.GetChild(0).localScale.x > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+
+                transform.GetChild(0).localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+
+                for (int i = 0; i < childs.Count; i++)
+                {
+                    childs[i].localScale-= new Vector3(0.1f, 0.1f, 0.1f);
+                }
+            }
+        }
+
+        yield return new WaitForSeconds(waitTime);
     }
 }
