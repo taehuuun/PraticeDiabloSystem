82b5df353a23f0031a05e26d6687a094c641df5f
taehuuun <lsw463@naver.com>
2023-08-17 04:20:15 +0900

    Add: 필요 필드 변수 추가

--- Assets/01. Scripts/UI/CameraFacing.cs
+++ Assets/01. Scripts/UI/CameraFacing.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
+public enum Axis { Up, Down, Left, Right, Forward}
+
 public class CameraFacing : MonoBehaviour
 {
+    private Camera _referenceCamera;
+    
+    public bool reverseFace = false;
+    public Axis axis = Axis.Up;
+    
 }
