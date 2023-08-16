bebd7a0e89223ca2f216edeed502fc47eb3b44e2
taehuuun <lsw463@naver.com>
2023-08-17 04:23:59 +0900

    Feat: GetAxis 메서드 구현
    Axis Enum값에 해당하는 벡터를 리턴하는 기능의 메서드를 구현

--- Assets/01. Scripts/UI/CameraFacing.cs
+++ Assets/01. Scripts/UI/CameraFacing.cs
@@ -8,5 +8,21 @@ public class CameraFacing : MonoBehaviour
     
     public bool reverseFace = false;
     public Axis axis = Axis.Up;
-    
+
+    public Vector3 GetAxis(Axis refAxis)
+    {
+        switch (refAxis)
+        {
+            case Axis.Down:
+                return Vector3.down;
+            case Axis.Left:
+                return Vector3.left;
+            case Axis.Right:
+                return Vector3.right;
+            case Axis.Forward:
+                return Vector3.forward;
+        }
+
+        return Vector3.up;
+    }
 }
