bbc6f0ec5dad523b18b2cd455e22052768d21097
taehuuun <lsw463@naver.com>
2023-08-17 10:34:47 +0900

    Feat: GaugeUI 클래스 내 MinValue 메서드 구현
    현재 게이지 UI의 최소 값을 반환하고, 세팅 하는 메서드를 구현

--- Assets/01. Scripts/UI/GaugeUI.cs
+++ Assets/01. Scripts/UI/GaugeUI.cs
@@ -4,4 +4,10 @@ using UnityEngine.UI;
 public class GaugeUI : MonoBehaviour
 {
     [SerializeField] private Slider gauge;
+        
+    public float MinValue
+    {
+        get => gauge.minValue;
+        set => gauge.minValue = value;
+    }
 }
